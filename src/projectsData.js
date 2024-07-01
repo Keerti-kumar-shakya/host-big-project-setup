cf9a6456205c0c5044f8199b34ea46ab482c511f
Keerti-kumar-shakya <keerti.shakya0008@gmail.com>
2024-07-01 16:04:28 +0530

    comfy store app uploaded and live

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,15 +1,16 @@
 // big project images
-import bImg_1 from './projects-images/YouTube-Clone-img-1.png';
+import bImg_1 from './projects-images/01-YouTube-Clone-img-1.png';
+import bImg_2 from './projects-images/02-Comfy-Store-app.png';
 import { nanoid } from 'nanoid';
 // small project images
 
 export const bigProjectData = [
+
+  // Project: 01;
   {
   id: nanoid(),
   category: 'HTML&&CSS',
   img: bImg_1,
-
-  projectNumber: 'first project',
 
   techUsed: 'HTML and CSS',
 
@@ -22,57 +23,43 @@ export const bigProjectData = [
   projectName: 'social media youTube clone'
 
   },
+    // Project: 02;
   {
     id: nanoid(),
-    category: 'vanilla-Js',
-    img: bImg_1,
-    projectNumber: 'first project',
-    techUsed: 'HTML and CSS',
-    desc: 'Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. Asperiores Aperiam Porro Impedit Tenetur Quo Hic Omnis Doloribus Dolores Enim Deleniti.',
-    url: '',
-    github: '',
-    projectName: 'social media youTube clone'
+    category: 'react-redux',
+    img: bImg_2,
+    techUsed: 'React JS, Redux Toolkit, Tailwind CSS, Daisy UI Tailwind, React Query, React Router DOM, React Toastify, Axios, Day.js, and React Icons',
+
+    desc: `The Comfy Store App is an advanced e-commerce web application designed to provide users with a seamless and enjoyable shopping experience. Utilizing a suite of modern web technologies, this project combines powerful functionality with a visually appealing and intuitive user interface. Key technologies used include React JS, Redux Toolkit, Tailwind CSS, Daisy UI Tailwind, React Query, React Router DOM, React Toastify, Axios, Day.js, and React Icons.
+    
+Key Features :-
+
+User Authentication: Secure user authentication and authorization system, enabling users to create accounts, log in, and manage their profiles.
+
+Product Catalog: Comprehensive product catalog with detailed product descriptions, images, and pricing, allowing users to browse and search for items easily.
+
+Shopping Cart: Interactive shopping cart functionality, enabling users to add, remove, and update items, with real-time updates and total price calculations.
+
+Checkout Process: Streamlined and secure checkout process, guiding users through order confirmation and payment steps.
+
+Responsive Design: Ensured the application is fully responsive, providing an optimal viewing experience on a wide range of devices and screen sizes.
+
+Real-Time Data Updates: Leveraged React Query to ensure real-time updates and synchronization of product data, cart status, and user information.
+
+User Notifications: Integrated React Toastify for real-time notifications, enhancing user interaction and feedback.
+
+Elegant UI: Combined Tailwind CSS and Daisy UI to create a visually appealing and consistent design, with a focus on usability and user experience.
+    `
+    ,
+    url: 'https://01-big-react-comfy-store-project.netlify.app/',
+
+    github: 'https://github.com/Keerti-kumar-shakya/big-react-redux-projects/tree/main/01-%20comfy%20store%20app',
+
+    projectName: 'comfy store app'
   
   },
 
-  {
-    id: nanoid(),
-    category: 'reactJs',
-    img: bImg_1,
-    projectNumber: 'first project',
-    techUsed: 'HTML and CSS ccbxcbx',
-    desc: 'Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. Asperiores Aperiam Porro Impedit Tenetur Quo Hic Omnis Doloribus Dolores Enim Deleniti.',
-    url: '',
-    github: '',
-    projectName: 'social media youTube clone'
-  
-  
-    },
-    {
-      id: nanoid(),
-      category: 'react-redux',
-      img: bImg_1,
-      projectNumber: 'first project',
-      techUsed: 'HTML and CSS dfdff',
-      desc: 'Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. Asperiores Aperiam Porro Impedit Tenetur Quo Hic Omnis Doloribus Dolores Enim Deleniti.',
-      url: '',
-      github: '',
-      projectName: 'social media youTube clone'
-    
-    },
-
-    {
-      id: nanoid(),
-      category: 'react-redux',
-      img: bImg_1,
-      projectNumber: 'first project',
-      techUsed: 'HTML and CSS dfdff',
-      desc: 'Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. Asperiores Aperiam Porro Impedit Tenetur Quo Hic Omnis Doloribus Dolores Enim Deleniti.',
-      url: '',
-      github: '',
-      projectName: 'social media youTube clone'
-    
-    }
+ 
 
 ]
 
